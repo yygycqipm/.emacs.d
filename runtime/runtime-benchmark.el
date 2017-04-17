a3dd7d6c4536678c9e11574537cf34c07d83e375
yunchenwen <9971064@qq.com>
2017-04-17 11:21:57 +0800

    'update'

--- runtime/runtime-benchmark.el
+++ runtime/runtime-benchmark.el
@@ -1,11 +1,11 @@
 ;; runtime-benchmark.el
 
-(defun time-subtract-ms (a b)
-  (truncate (* 1000 (float-time (time-subtract a b)))))
+(defun time-subtract-sec (a b)
+  (format "%.2f" (float-time (time-subtract a b))))
   
 (defun print-init-time ()
-  (message "init completed in %s ms"
-           (time-subtract-ms after-init-time before-init-time)))
+  (message "Finish: %s s"
+           (time-subtract-sec after-init-time before-init-time)))
       
 (add-hook 'after-init-hook 'print-init-time)
 
--- runtime/runtime-lib.el
+++ runtime/runtime-lib.el
@@ -1,5 +1,7 @@
 ;; runtime-lib.el
 
-(require-pkg 'cl-lib)
+;(require-pkg 'cl-lib)
+
+(provide 'runtime-lib)
 
 ;; runtime-lib.el ends here
