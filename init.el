fecc91e83d7c3dbd9402458e51971a5a4bdca585
hanhouxiong <hhx@hhx.com>
2019-12-02 19:51:28 +0800

    fixed

--- init.el
+++ init.el
@@ -1,15 +1,10 @@
-;;;                                          __
-;;      ___  ____ ___  ____ ___________ ____/ /
-;;     / _ \/ __ `__ \/ __ `/ ___/ ___// __  / 
-;;   _/  __/ / / / / / /_/ / /__(__  )/ /_/ /  
-;;  (_)___/_/ /_/ /_/\__,_/\___/____(_)__,_/                                              
-;;;
-;;  Author: Yunchen Wen <yunchenplz@gmail.com>
-;;;
+;;; init.el -*- coding: utf-8; lexical-binding: t -*- 
 
+;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
+;;(setq debug-on-error t)
 
 ;;
-; Optimize garbage collection
+; Adjust garbage collection thresholds during startup, and thereafter
 ;;
 (defconst gc-cons-threshold' gc-cons-threshold)
 (setq gc-cons-threshold (* 128 1024 1024))
@@ -57,3 +52,8 @@
 (when (file-exists-p custom-file) (load custom-file))
   
 (provide 'init)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
+;;; init.el ends here
